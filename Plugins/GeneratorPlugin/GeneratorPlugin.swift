44a60f46a8b47bc5fc8b0227fbe381665ce6744f
Mikhail Tishin <michail.tishin@fayrix.com>
2023-09-30 11:51:15 +0300

    Fix plugin execution on Windows

--- Plugins/GeneratorPlugin/GeneratorPlugin.swift
+++ Plugins/GeneratorPlugin/GeneratorPlugin.swift
@@ -6,13 +6,21 @@ import PackagePlugin
         let executable: Path = try context.tool(named: "Generator").path
         let directory: Path = context.pluginWorkDirectory.appending(subpath: "GeneratedScources")
         let output: Path = directory.appending(subpath: "Product.swift")
-
+        
+        #if os(Windows)
+        let command: Command = Command.prebuildCommand(
+            displayName: "Generating",
+            executable: executable,
+            arguments: [directory],
+            outputFilesDirectory: directory)
+        #else
         let command: Command = Command.buildCommand(
             displayName: "Generating",
             executable: executable,
             arguments: [directory],
             inputFiles: [],
             outputFiles: [output])
+        #endif
         
         return [command]
     }
